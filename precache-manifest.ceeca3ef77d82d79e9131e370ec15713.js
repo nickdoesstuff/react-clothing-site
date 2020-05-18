076423be8c8178680e60f7fc56e5653bc6ecc131
Nick Shelton <nickshelton1376@gmail.com>
2020-05-18 14:59:32 -0400

    Updates

---
+++ precache-manifest.ceeca3ef77d82d79e9131e370ec15713.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96720317a17acb5c47a0549a2178e37",
+    "url": "/react-clothing-site/index.html"
+  },
+  {
+    "revision": "f680de17e8a00007a0b9",
+    "url": "/react-clothing-site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9b06919b17927867386a",
+    "url": "/react-clothing-site/static/js/2.79a4dcf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clothing-site/static/js/2.79a4dcf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f680de17e8a00007a0b9",
+    "url": "/react-clothing-site/static/js/main.f7dd618c.chunk.js"
+  },
+  {
+    "revision": "dae5b0839f2f50ca6a7e",
+    "url": "/react-clothing-site/static/js/runtime-main.d67f19ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-clothing-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
